452ef2948a6e4444dd59f0e61df29a0a3580a8ae
Jack Marley Payne <jack.marleypayne@gmail.com>
2019-10-11 14:09:04 -0400

    10.11 updates

--- college_data.R
+++ college_data.R
@@ -1,26 +1,4 @@
-library(tidyverse)
-library(devtools)
-devtools::install_github("UrbanInstitute/education-data-package-r")
-library(educationdata)
-df<-get_education_data(level = 'college-university', 
-                       source = 'ipeds', 
-                       topic = 'admissions-requirements',
-                       filters=list(year=1997:2008),
-                       add_labels = TRUE)
-df2<-get_education_data(level = 'college-university', 
-                       source = 'scorecard', 
-                       topic = 'earnings',
-                       filters=list(year=2004:2014),
-                       add_labels = TRUE)
-df2<-df2 %>% filter(years_after_entry==6|years_after_entry==7)
-
-df <- df %>% unite(match,unitid,year,remove=FALSE)
-
-df2 <- df2 %>% unite(match,unitid,cohort_year,remove=FALSE)
-
-df_full <- df %>% right_join(df2,by="match")
-
-
+df_full<-read.csv("df_full.csv")
 
 df25<-df_full%>% filter(earnings_mean>0&sat_math_25_pctl>0)
 
@@ -50,3 +28,33 @@ dfclean<-df_full %>% filter_at(vars(sat_math_25_pctl,sat_crit_read_25_pctl,sat_w
 
 sat_earnings_regress<-lm(earnings_mean~sat_crit_read_25_pctl+sat_writing_25_pctl+sat_math_25_pctl,data=dfclean)
 summary(sat_earnings_regress)
+
+sat_math_av<-mean(df25$sat_math_25_pctl)
+sat_math_sd<-sd(df25$sat_math_25_pctl)
+
+sat_crit_av<-mean(dfcrit$sat_crit_read_25_pctl)
+sat_crit_sd<-sd(dfcrit$sat_crit_read_25_pctl)
+
+sat_writ_av<-mean(dfwrit$sat_writing_25_pctl)
+sat_writ_sd<-sd(dfwrit$sat_writing_25_pctl)
+
+df_act_math<- df_full%>% filter(earnings_mean>0&act_math_25_pctl>0)
+
+df_act_math %>% ggplot(aes(x=earnings_mean,y=act_math_25_pctl))+
+  geom_point()+
+  geom_smooth(method=lm)
+
+df_act_english<- df_full%>% filter(earnings_mean>0&act_english_25_pctl>0)
+
+act_math_av<-mean(df_act_math$act_math_25_pctl)
+act_math_sd<-sd(df_act_math$act_math_25_pctl)
+
+act_english_av<-mean(df_act_english$act_english_25_pctl)
+act_english_sd<-sd(df_act_english$act_english_25_pctl)
+
+df_act_english %>% ggplot(aes(x=earnings_mean,y=act_english_25_pctl))+
+  geom_point()+
+  geom_smooth(method=lm)
+
+act_earnings_regress<-lm(earnings_mean~act_math_25_pctl+act_english_25_pctl,data=df_act_math)
+summary(act_earnings_regress)
--- college_wrangle.R
+++ college_wrangle.R
@@ -2,16 +2,38 @@ library(tidyverse)
 library(devtools)
 devtools::install_github("UrbanInstitute/education-data-package-r")
 library(educationdata)
-df<-get_education_data(level = 'college-university', 
+
+admit_df<-get_education_data(level = 'college-university', 
 source = 'ipeds', 
 topic = 'admissions-requirements',
-filters=list(year=1997:2008),
+filters=list(year=1997:2014),
 add_labels = TRUE)
-df2<-get_education_data(level = 'college-university', 
+
+earnings_df<-get_education_data(level = 'college-university', 
 source = 'scorecard', 
 topic = 'earnings',
 filters=list(year=2004:2014),
 add_labels = TRUE)
+
+repay_df<-get_education_data(level = 'college-university', 
+                          source = 'scorecard', 
+                          topic = 'repayment',
+                          filters=list(year=2015:2016),
+                          add_labels = TRUE)
+
+grad_df<-get_education_data(level = 'college-university', 
+                             source = 'ipeds', 
+                             topic = 'grad-rates',
+                             filters=list(year=2006:2016),
+                             add_labels = TRUE)
+
+grad_final<-grad_df %>%  filter(race=='Total'&sex=='Total'&subcohort=='Total')
+
+write.csv(admit_df,"admit.csv")
+write.csv(earnings_df,"earnings.csv")
+write.csv(repay_df,"repay.csv")
+write.csv(grad_final,"grad.csv")
+
 df3<-df2 %>% filter(years_after_entry==6|years_after_entry==7)
 
 df4 <- df %>% unite(match,unitid,year,remove=FALSE)
